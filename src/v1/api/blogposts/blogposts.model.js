a242fab30d9f4d202ead311a3934675c4d8cf3f4
melitus <asmelitus@gmail.com>
2020-05-26 20:59:08 +0100

    add blogpost model

---
+++ src/v1/api/blogposts/blogposts.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+let Schema = mongoose.Schema
+
+const blogpostsSchema = new Schema ({
+    title: {type: String},
+    content: {type: String},
+    comments: [{type: Schema.ObjectId, ref: 'Comments'}]
+})
+
+module.exports = mongoose.model("Users", blogpostsSchema)
